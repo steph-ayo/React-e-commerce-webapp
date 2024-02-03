7148ac8b25acffdbc61ad0ef4e2f1c1a303b9c91
steph-ayo <popoolastephen19@gmail.com>
2024-02-03 23:00:51 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 style={{ color: "red" }}>Hello, Developer!</h1>
+    </div>
+  );
+}
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+import "swiper/css";
+
+// bootstrap css
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+// fonts and icons
+import "././assets/css/icofont.min.css";
+import "././assets/css/animate.css";
+import "././assets/css/style.min.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
