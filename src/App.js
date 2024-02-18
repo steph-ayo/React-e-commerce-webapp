2fb84a83b0207ec59f537a654e033f0908c11b23
steph-ayo <popoolastephen19@gmail.com>
2024-02-18 23:23:39 +0100

    navitems.js update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import NavItems from "./components/Navbar/NavItems";
+import NavItems from "./components/NavItems";
 
 export default function App() {
   return (
