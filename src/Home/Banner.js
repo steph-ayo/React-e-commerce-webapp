4f9aa0b45a1a69c767a6e769ac8645e5c42b6690
steph-ayo <popoolastephen19@gmail.com>
2024-03-14 11:38:52 +0100

    HomeCategory.js update

--- src/Home/Banner.js
+++ src/Home/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import productData from "../products.json";
 import { Link } from "react-router-dom";
+import SelectedCategory from "../components/SelectedCategory";
 
 const title = (
   <h2>
@@ -47,6 +48,7 @@ export default function Banner() {
         <div className="banner-content">
           {title}
           <form>
+            <SelectedCategory select={"all"} />
             <input
               type="text"
               name="search"
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import HomeCategory from "./HomeCategory";
 
 export default function Home() {
   return (
     <div>
       <Banner />
+      <HomeCategory />
     </div>
   );
 }
